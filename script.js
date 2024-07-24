33523f672ec1cce2650f143986a71fde1a94ab34
aparnashibu12 <aparnashibu520@gamil.com>
2024-07-24 13:49:25 +0530

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Replace 'your_api_key' with your actual API key from OpenWeatherMap
+
 const apiKey = '81e215eca73c3ce648c3454afb441e90';
 
-// Select elements from the DOM
+
 const cityInput = document.getElementById('cityInput');
 const getWeatherBtn = document.getElementById('getWeatherBtn');
 const weatherInfo = document.getElementById('weatherInfo');
 
-// Event listener for button click
+
 getWeatherBtn.addEventListener('click', () => {
     const city = cityInput.value.trim();
 
-    // Validate input
+    
     if (city.length === 0) {
         alert('Please enter a city name');
         return;
     }
 
-    // API URL
+    
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=thiruvananthapuram&appid=81e215eca73c3ce648c3454afb441e90&units=metric' ;
 
-    // Fetch weather data
+    
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
@@ -28,7 +28,7 @@ getWeatherBtn.addEventListener('click', () => {
             return response.json();
         })
         .then(data => {
-            // Display weather information
+            
             const temperature = data.main.temp;
             const description = data.weather[0].description;
 
